c1e2275d3fd2cf4fd7327f3999f7fabd57d60005
md-alamin2 <mdalamin22671@gmail.com>
2026-05-20 23:39:14 +0600

    primitive data type in ts

---
+++ src/primitive.ts
@@ -0,0 +1,10 @@
+// JS -> TS
+// string, number, boolean, undefined, null.
+
+// TS -> never, unknown, void
+
+let userName: string = "Al-amin";
+const userAge: number = 25;
+const isAdmin: boolean = true;
+
+userName = 123; // Error: Type 'number' is not assignable to type 'string'.
