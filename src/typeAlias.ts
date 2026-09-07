b2ab02f8882651ea7131df61afd6c6e61e3f1b9d
md-alamin2 <mdalamin22671@gmail.com>
2026-09-07 12:57:31 +0600

    learn about type alias in object and function

---
+++ src/typeAlias.ts
@@ -0,0 +1,42 @@
+//? type alias
+
+
+// ? type alias in object
+type User = {
+  id: number;
+  name: {
+    firstName: string;
+    lastName: string;
+  };
+  gender: "male" | "female";
+  contactNo: string;
+  address: {
+    division: string;
+    district: string;
+    upazila: string;
+    postalCode: number;
+  };
+};
+
+const user: User = {
+  id: 1234,
+  name: {
+    firstName: "Md",
+    lastName: "Al-amin",
+  },
+  contactNo: "017777777777",
+  address: {
+    division: "Dhamsona",
+    district: "Dhaka",
+    upazila: "Savar",
+    postalCode: 12345,
+  },
+};
+
+
+
+//? type alias in function
+
+type addFunc= (num1: number, num2: number)=> number
+
+const add: addFunc = (num1, num2)=> num1+num2
