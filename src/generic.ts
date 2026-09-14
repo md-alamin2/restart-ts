638f9178c785e227b8b329394f6b841275f337de
md-alamin2 <mdalamin22671@gmail.com>
2026-09-14 20:53:26 +0600

    learn generic with interface

--- src/generic.ts
+++ src/generic.ts
@@ -1,8 +1,6 @@
 // generics type => dynamically generalize
 
-
-
-type GenericArray<T> = Array<T>
+type GenericArray<T> = Array<T>;
 
 // const friend: string[] = ["a", "b", "c", "d", "e"];
 const friend: GenericArray<string> = ["a", "b", "c", "d", "e"];
@@ -13,23 +11,86 @@ const roll: GenericArray<number> = [1, 2, 3, 4];
 // const isEligible: boolean[] = [true, false, true];
 const isEligible: GenericArray<boolean> = [true, false, true];
 
-
 // type Coordinates = [number, number] //type alias
-type Coordinates<X, Y> = [X, Y] // generalize 
+type Coordinates<X, Y> = [X, Y]; // generalize
 
-const coordinates1: Coordinates<number, number> = [1, 2]
-const coordinates2: Coordinates<string, string> = ['1', '2']
+const coordinates1: Coordinates<number, number> = [1, 2];
+const coordinates2: Coordinates<string, string> = ["1", "2"];
 
+// ? generic for array of object
 
-// ? generic for array of object 
+const userList: GenericArray<{ name: string; age: number }> = [
+  {
+    name: "Al-amin",
+    age: 22,
+  },
+  {
+    name: "Al-amin",
+    age: 22,
+  },
+];
 
-const userList : GenericArray<{name: string, age: number}>=[
-    {
-       name: "Al-amin",
-       age: 22 
-    },
-    {
-       name: "Al-amin",
-       age: 22 
-    },
-]
+// ? generic with interface
+
+interface Developer<T, B = null> {
+  name: string;
+  salary: number;
+  device: {
+    brand: string;
+    model: string;
+    releasedYear: string;
+  };
+  smartWatch: T;
+  bike?: B;
+}
+
+interface nonBrandedWatch {
+  heartRate: string;
+  stopWatch: boolean;
+}
+
+interface brandedWatch {
+  heartRate: string;
+  stopWatch: boolean;
+  callingFeature: boolean;
+  calculator: boolean;
+}
+
+interface Bike {
+  model: string;
+  cc: number;
+}
+
+const poorDeveloper: Developer<nonBrandedWatch, Bike> = {
+  name: "Mr. Developer",
+  salary: 20000,
+  device: {
+    brand: "lenovo",
+    model: "A21",
+    releasedYear: "2016",
+  },
+  smartWatch: {
+    heartRate: "Yes",
+    stopWatch: true,
+  },
+};
+
+const richDeveloper: Developer<brandedWatch, Bike> = {
+  name: "Mr. Developer",
+  salary: 20000,
+  device: {
+    brand: "lenovo",
+    model: "A21",
+    releasedYear: "2016",
+  },
+  smartWatch: {
+    heartRate: "Yes",
+    stopWatch: true,
+    callingFeature: true,
+    calculator: true,
+  },
+  bike: {
+    model: "Yamaha",
+    cc: 150,
+  },
+};
